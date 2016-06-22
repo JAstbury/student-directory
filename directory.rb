13b7221192adb085504a0188ac684d5744412a8b
Jess Astbury <jess.astbury@hotmail.com>
2016-06-22 14:41:15 +0000

    Refactored code by separating out methods

--- directory.rb
+++ directory.rb
@@ -1,25 +1,38 @@
+@students = []
 
 def interactive_menu
-$students = []
   loop do
+    print_menu
+    process(gets.chomp)
+   end
+end
+
+def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit"
-    selection = gets.chomp
+end
+
+def process(selection)
     case selection
         when "1"
-            $students = input_students
+            input_students
         when "2"
-            print_header
-            print_names($students)
-            print_footer($students)
+            show_students
         when "9"
             exit
-    else
-        puts "Please enter a valid option."
+        else
+            puts "Please enter a valid option."
     end
 end
+
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
 end
+    
 
 def input_students
     puts "Please enter the names of the students"
@@ -29,8 +42,8 @@ def input_students
         puts "What's cohort(month) is #{name} in?"
             month = gets.gsub("\n", "").capitalize
             month = "November" if month == ""
-            $months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-            if $months.include?(month) == false 
+            @months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+            if @months.include?(month) == false 
                 puts "Please enter a correct month"
                 month = gets.gsub("\n", "")
             end
@@ -41,36 +54,33 @@ def input_students
         puts "How tall(cm) is #{name}?"
             height = gets.gsub("\n", "")
             height = "N/A" if height == ""
-        puts "What's #{name}'s hobbies?"
-            hobbies = gets.gsub("\n", "")
-            hobbies = "N/A" if hobbies == ""
-        $students << {name: name, cohort: cohort, dob: date, height: height, hobbies: hobbies}
-            if $students.count > 1
-                puts "Now we have #{$students.count} students"
+        @students << {name: name, cohort: cohort, dob: date, height: height}
+            if @students.count > 1
+                puts "Now we have #{@students.count} students"
             else
-                puts "Now we have #{$students.count} student"
+                puts "Now we have #{@students.count} student"
             end
         name = gets.gsub("\n", "")
     end
-    $students
 end
 
 
 def print_header
-    puts "The students of Villains Academy".center(60)
-    puts "-------------".center(60)
+    puts "The Students of Villains Academy".center(100,".")
+    puts "..........".center(100,".")
+    puts
 end
 
 
-def print_names(students)
-if students.empty?
-    puts "No students have been inputted".center(60)
+def print_students_list
+if @students.empty?
+    puts "No students have been inputted".center(100)
 else
 index = 1
-$months.each do |month|
-    students.each do |student|
+@months.each do |month|
+    @students.each do |student|
         if student[:cohort] == month
-           puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort).........hobbies = #{student[:hobbies]}".center(60)
+           puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort).........DOB: #{student[:dob]}".center(100)
             index += 1
         end
     end
@@ -79,12 +89,12 @@ end
 end
 
 
-def print_footer(students)
-    if !students.empty?
-    if students.count == 1
-        puts "Overall, we have #{students.count} great student".center(60)
+def print_footer
+    if !@students.empty?
+    if @students.count == 1
+        puts "\n" + "Overall, we have #{@students.count} great student".center(100)
     else
-        puts "Overall, we have #{students.count} great students".center(60)
+        puts "\n" + "Overall, we have #{@students.count} great students".center(100)
     end
     end
 end
